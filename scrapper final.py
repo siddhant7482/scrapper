2947bf18d171e687410caded8388d28ccf721a45
Siddhant Bhasin <75569469+siddhant7482@users.noreply.github.com>
2023-03-22 11:25:27 +0530

    Update scrapper final.py

--- scrapper final.py
+++ scrapper final.py
@@ -5,46 +5,50 @@ Created on Fri Mar 10 11:04:13 2023
 @author: siddh
 """
 
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 13 22:36:34 2023
+
+@author: siddh
+"""
 import instaloader
-import re
-import numpy as np
-import pandas as pd
-from itertools import dropwhile, takewhile
-import csv
-def userinfo(topic):
-    bot = instaloader.Instaloader()
-    bot.login('python9174','Siddhant@7482')
-    search_results = instaloader.TopSearchResults(bot.context, topic)
-    for profile in search_results.get_profiles():
-            if (profile.followers>=2000):
-             email=re.findall(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b", profile.biography)
-             username=profile.username
-             User_ID=profile.userid
-             Number_of_Posts=profile.mediacount
-             FollowersCount=profile.followers
-             FollowingCount=profile.followees
-             Bio= profile.biography
-             link=profile.external_url
-             data=[]
-             user_data={'Username':[username],'Name':[User_ID],'number of posts':[Number_of_Posts],'followers count':[FollowersCount],'following count':[FollowingCount],'bio':[Bio],'link':[link]}
-             data.append(user_data)         
-             df=pd.DataFrame(data)
-        
-            ''' data={profile.username, profile.userid,profile.mediacount,profile.followers,profile.followees,profile.biography,profile.external_url}
-             print(data)
-            
-             print(profile)
-             print("Username: ", profile.username)
-             print("User ID: ", profile.userid)
-             print("Number of Posts: ", profile.mediacount)
-             print("Followers Count: ", profile.followers)
-             print("Following Count: ", profile.followees)
-             print("Bio: ", profile.biography)
-             print("External URL: ", profile.external_url)
-             emails = re.findall(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b", profile.biography)
-             print("Emails extracted from the bio:")
-             print(emails)
-             '''
-            print(df)     
-userinfo("Toys")
-        
+import openpyxl
+
+# Create an instance of Instaloader class
+L = instaloader.Instaloader()
+#L.login('python9174','Siddhant@7482')
+
+# Define the hashtag to search for
+hashtag = input("Enter the hashtag to search for: ")
+
+# Get the Hashtag object for the specified hashtag
+hashtag_obj = instaloader.Hashtag.from_name(L.context, hashtag)
+
+# Load the workbook if it exists, otherwise create a new one
+try:
+    wb = openpyxl.load_workbook("instagram_data.xlsx")
+    ws = wb.active
+except FileNotFoundError:
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.append(["Username", "Userid", "Total posts", "Followers", "Following", "Bio", "Profile link"])
+
+# Set the maximum number of posts to scrape
+max_posts = 30
+
+# Loop through all the recent posts for the hashtag
+for post in hashtag_obj.get_posts():
+    username = post.owner_username
+    profile = instaloader.Profile.from_username(L.context, username)
+    
+    # Check if the user has more than 2000 followers before scraping their data
+    if profile.followers >= 2000:
+        # Write the scraped data to the Excel file
+        ws.append([profile.username, profile.userid, profile.mediacount, profile.followers, profile.followees, profile.biography, profile.external_url])
+
+        # Exit the loop if we have scraped the maximum number of posts
+        if ws.max_row >= max_posts:
+            break
+
+# Save the changes to the Excel file
+wb.save("Games.xlsx")
